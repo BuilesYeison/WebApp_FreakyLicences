fa489eb5e4fb629cde39ccd35ebb8bd19ab0bc5d
zeu <zeu@pop-os.localdomain>
2022-03-27 16:56:45 -0500

    Img can be modified now, design updated

--- src/app/licenses/default/default.component.ts
+++ src/app/licenses/default/default.component.ts
@@ -9,6 +9,7 @@ export class DefaultComponent implements OnInit {
   name:string = "Pedrito Perez";
   note:string = "Es otaku juega al lol y se baña una vez al mes!";
   date:string = "2001-07-15";
+  imgUrl:string = "https://i.imgur.com/HXY3V2U.jpeg";
   constructor() { }
 
   ngOnInit(): void {
